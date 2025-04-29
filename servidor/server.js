b0798caba2fc9f4d36567bc2615c9ffff67b5cb6
BeDaLara <bernardosilvacorlaite2406@gmail.com>
2025-04-29 07:53:56 -0300

    arrumaaaa

--- servidor/server.js
+++ servidor/server.js
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS // use uma senha de app, não a principal
+  pass: process.env.EMAIL_PASS // use uma senha de app, não a principal
     }
 });
 
